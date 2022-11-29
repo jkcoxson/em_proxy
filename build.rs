f94d9ff2c1186fcad5b6b1b7a87dd319b4b9d144
JJTech0130 <jjtech@jjtech.dev>
2022-11-28 19:15:12 -0500

    use cbindgen rust-style rather then via command

--- build.rs
+++ build.rs
@@ -1,19 +1,11 @@
 // Jackson Coxson
-
-use std::process::Command;
+use std::env;
+use cbindgen;
 
 fn main() {
-    // Generate the cbindings
-    let _ = if cfg!(target_os = "windows") {
-        Command::new("cmd")
-            .args(["/C", "cbindgen -c cbindgen.toml -o em_proxy.h"])
-            .output()
-            .expect("Failed to generate c bindings")
-    } else {
-        Command::new("sh")
-            .arg("-c")
-            .arg("cbindgen -c cbindgen.toml -o em_proxy.h")
-            .output()
-            .expect("Failed to generate c bindings")
-    };
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    cbindgen::generate(crate_dir)
+      .expect("Unable to generate C bindings")
+      .write_to_file("em_proxy.h");
 }
